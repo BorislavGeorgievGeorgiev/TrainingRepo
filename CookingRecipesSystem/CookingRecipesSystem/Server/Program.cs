cf18982bb264b4634ce3f829b2083665bd010f25
Borislav <Borislav.G.Georgiev@gmail.com>
2022-04-20 14:40:18 +0300

    Identity scaffolding for Login and Register.

--- CookingRecipesSystem/CookingRecipesSystem/Server/Program.cs
+++ CookingRecipesSystem/CookingRecipesSystem/Server/Program.cs
@@ -1,16 +1,27 @@
 using CookingRecipesSystem.Server.Data;
 using CookingRecipesSystem.Server.Models;
+using CookingRecipesSystem.Shared.Common;
 
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+/*var connectionString = builder.Configuration.GetConnectionString("CookingRecipesSystemServerContextConnection");;
+
+builder.Services.AddDbContext<CookingRecipesSystemServerContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<CookingRecipesSystemServerContext>();;*/
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -18,6 +29,17 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
 
 builder.Services.AddIdentityServer()
     .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+  options.Password.RequireNonAlphanumeric = false;
+  options.Password.RequireDigit = false;
+  options.Password.RequireUppercase = false;
+  options.Password.RequireLowercase = false;
+  options.Password.RequireUppercase = false;
+  options.Password.RequiredUniqueChars = IdentityConstantsCRS.PasswordUniqueCharsCount;
+  options.Password.RequiredLength = IdentityConstantsCRS.PasswordMinLength;
+});
 
 builder.Services.AddAuthentication()
     .AddIdentityServerJwt();
