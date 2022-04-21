368e434d5be3ea1be75ebacdeeb9b9150dea1939
Borislav <Borislav.G.Georgiev@gmail.com>
2022-04-21 16:23:08 +0300

    Start new project without "Identity"

--- CookingRecipesSystem/CookingRecipesSystem/Server/Models/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace CookingRecipesSystem.Server.Models
-{
-  public class ApplicationUser : IdentityUser
-  {
-  }
-}
--- CookingRecipesSystem/CookingRecipesSystem/Server/Program.cs
+++ CookingRecipesSystem/CookingRecipesSystem/Server/Program.cs
@@ -1,48 +1,8 @@
-using CookingRecipesSystem.Server.Common;
-using CookingRecipesSystem.Server.Data;
-using CookingRecipesSystem.Server.Models;
-
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
-/*var connectionString = builder.Configuration.GetConnectionString("CookingRecipesSystemServerContextConnection");;
-
-builder.Services.AddDbContext<CookingRecipesSystemServerContext>(options =>
-    options.UseSqlServer(connectionString));;
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<CookingRecipesSystemServerContext>();;*/
-
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddIdentityServer()
-    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-  options.Password.RequireNonAlphanumeric = false;
-  options.Password.RequireDigit = false;
-  options.Password.RequireUppercase = false;
-  options.Password.RequireLowercase = false;
-  options.Password.RequireUppercase = false;
-  options.Password.RequiredUniqueChars = IdentityConstantsCRS.PasswordUniqueCharsCount;
-  options.Password.RequiredLength = IdentityConstantsCRS.PasswordMinLength;
-});
-
-builder.Services.AddAuthentication()
-    .AddIdentityServerJwt();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
@@ -52,7 +12,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseMigrationsEndPoint();
   app.UseWebAssemblyDebugging();
 }
 else
@@ -68,10 +27,6 @@ app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseIdentityServer();
-app.UseAuthentication();
-app.UseAuthorization();
 
 
 app.MapRazorPages();
